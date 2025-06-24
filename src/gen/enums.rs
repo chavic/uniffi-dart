b54cf9787133a329fc686393c249b47cadd25a33
spacebear <git@spacebear.dev>
2025-06-23 21:49:09 -0400

    Fix empty field names for Enum tuple variants
    Previously unnamed tuple variants would generate fields with the empty
    string as a name, as in:
    
    class V2ReceiverException extends ReceiverException {
        final SessionError ;
        V2ReceiverException(this.,);
        V2ReceiverException._(this.,);
        // ...
    }
    
    This fix follows the approach from
    https://github.com/mozilla/uniffi-rs/blob/1d412109db2b29c44b80231a8438014236935127/uniffi_bindgen/src/bindings/kotlin/templates/macros.kt#L136
    with a helper function that uses tuple indices when the field.name() is
    empty.
    
    The helper functions replace "Error" with "Exception" to abide by
    DartCodeOracle::class_name.
    
    The constructor parameters now also use static typing.
    
    Co-authored-by: benalleng <benalleng@gmail.com>

--- src/gen/enums.rs
+++ src/gen/enums.rs
@@ -1,7 +1,7 @@
 use genco::prelude::*;
 use crate::gen::CodeType;
 use uniffi_bindgen::backend::Literal;
-use uniffi_bindgen::interface::{AsType, Enum};
+use uniffi_bindgen::interface::{AsType, Enum, Field};
 
 use super::oracle::{AsCodeType, DartCodeOracle};
 use super::render::{AsRenderable, Renderable, TypeHelperRenderer};
@@ -88,6 +88,17 @@ pub fn generate_enum(obj: &Enum, type_helper: &dyn TypeHelperRenderer) -> dart::
     } else {
         let mut variants = vec![];
 
+        // helper functions to get the sanitized field name and type strings
+        fn field_name(field: &Field, field_num: usize) -> String {
+            if field.name().is_empty() { format!("v{}", field_num) } else { DartCodeOracle::var_name(field.name()) }
+        }
+        fn field_type(field: &Field, type_helper: &dyn TypeHelperRenderer) -> String {
+            field.as_type().as_renderable().render_type(&field.as_type(), type_helper).to_string().expect("Could not stringify type").replace("Error", "Exception")
+        }
+        fn field_ffi_converter_name(field: &Field) -> String {
+            field.as_type().as_codetype().ffi_converter_name().replace("Error", "Exception")
+        }
+
         for (index, variant_obj) in obj.variants().iter().enumerate() {
             for f in variant_obj.fields() {
                 type_helper.include_once_check(&f.as_codetype().canonical_name(), &f.as_type());
@@ -95,13 +106,13 @@ pub fn generate_enum(obj: &Enum, type_helper: &dyn TypeHelperRenderer) -> dart::
             let variant_dart_cls_name = &format!("{}{}", DartCodeOracle::class_name(variant_obj.name()), dart_cls_name);
             
             // Prepare constructor parameters
-            let constructor_params = variant_obj.fields().iter().map(|field| {
-                let param_name = DartCodeOracle::var_name(field.name());
-                let _param_type = field.as_type().as_renderable().render_type(&field.as_type(), type_helper);
+            let constructor_params = variant_obj.fields().iter().enumerate().map(|(i, field)| {
+                let param_name = field_name(field, i);
+                let param_type = field_type(field, type_helper);
                 if variant_obj.fields().len() > 1 {
-                    quote!(required this.$param_name)
+                    quote!(required $param_type this.$param_name)
                 } else {
-                    quote!(this.$param_name)
+                    quote!($param_type this.$param_name)
                 }
             }).collect::<Vec<_>>();
             
@@ -113,24 +124,24 @@ pub fn generate_enum(obj: &Enum, type_helper: &dyn TypeHelperRenderer) -> dart::
             
             variants.push(quote!{
                 class $variant_dart_cls_name extends $dart_cls_name {
-                    $(for field in variant_obj.fields() => final $(&field.as_type().as_renderable().render_type(&field.as_type(), type_helper)) $(DartCodeOracle::var_name(field.name()));  )
+                    $(for (i, field) in variant_obj.fields().iter().enumerate() => final $(field_type(field, type_helper)) $(field_name(field, i));  )
                     
                     // Add the public const constructor
                     $variant_dart_cls_name($constructor_param_list);
 
                     // Keep the private constructor used by `read`
-                    $variant_dart_cls_name._($(for field in variant_obj.fields() => this.$(DartCodeOracle::var_name(field.name())), ));
+                    $variant_dart_cls_name._($(for (i, field) in variant_obj.fields().iter().enumerate() => $(field_type(field, type_helper)) this.$(field_name(field, i)), ));
 
                     static LiftRetVal<$variant_dart_cls_name> read( Uint8List buf) {
                         int new_offset = buf.offsetInBytes;
 
-                        $(for f in variant_obj.fields() =>
-                            final $(DartCodeOracle::var_name(f.name()))_lifted = $(f.as_type().as_codetype().ffi_converter_name()).read(Uint8List.view(buf.buffer, new_offset));
-                            final $(DartCodeOracle::var_name(f.name())) = $(DartCodeOracle::var_name(f.name()))_lifted.value;
-                            new_offset += $(DartCodeOracle::var_name(f.name()))_lifted.bytesRead;
+                        $(for (i, field) in variant_obj.fields().iter().enumerate() =>
+                            final $(field_name(field, i))_lifted = $(field_ffi_converter_name(field)).read(Uint8List.view(buf.buffer, new_offset));
+                            final $(field_name(field, i)) = $(field_name(field, i))_lifted.value;
+                            new_offset += $(field_name(field, i))_lifted.bytesRead;
                         )
                         return LiftRetVal($variant_dart_cls_name._(
-                            $(for f in variant_obj.fields() => $(DartCodeOracle::var_name(f.name())),)
+                            $(for (i, field) in variant_obj.fields().iter().enumerate() => $(field_name(field, i)),)
                         ), new_offset);
                     }
 
@@ -143,7 +154,7 @@ pub fn generate_enum(obj: &Enum, type_helper: &dyn TypeHelperRenderer) -> dart::
 
                     @override
                     int allocationSize() {
-                        return $(for f in variant_obj.fields() => $(f.as_type().as_codetype().ffi_converter_name()).allocationSize($(DartCodeOracle::var_name(f.name()))) + ) 4;
+                        return $(for (i, field) in variant_obj.fields().iter().enumerate() => $(field_ffi_converter_name(field)).allocationSize($(field_name(field, i))) + ) 4;
                     }
 
                     @override
@@ -151,8 +162,8 @@ pub fn generate_enum(obj: &Enum, type_helper: &dyn TypeHelperRenderer) -> dart::
                         buf.buffer.asByteData(buf.offsetInBytes).setInt32(0, $(index + 1)); // write index into first position;
                         int new_offset = buf.offsetInBytes + 4;
 
-                        $(for f in variant_obj.fields() =>
-                        new_offset += $(f.as_type().as_codetype().ffi_converter_name()).write($(DartCodeOracle::var_name(f.name())), Uint8List.view(buf.buffer, new_offset));
+                        $(for (i, field) in variant_obj.fields().iter().enumerate() =>
+                        new_offset += $(field_ffi_converter_name(field)).write($(field_name(field, i)), Uint8List.view(buf.buffer, new_offset));
                         )
 
                         return new_offset;
