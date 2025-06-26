b9e221a78c6dca61b5c616fb4a48d4f583af75f4
spacebear <git@spacebear.dev>
2025-06-26 16:06:07 -0400

    Fix Record rendering in callbacks

--- src/gen/oracle.rs
+++ src/gen/oracle.rs
@@ -403,6 +403,7 @@ impl DartCodeOracle {
                     quote!(RustBuffer $arg_name)
                 }
             },
+            Type::Record { .. } => quote!(RustBuffer $arg_name),
             _ => {
                 let type_label = DartCodeOracle::dart_type_label(Some(arg_type));
                 quote!($type_label $arg_name)
