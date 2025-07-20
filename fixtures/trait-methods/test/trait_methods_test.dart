614c52fad502ea2fe4cc6044c15e3893deb8fc80
chavic <dev.chavic@gmail.com>
2025-07-20 16:07:18 +0200

    bringing back blocks, using skip method instead

--- fixtures/trait-methods/test/trait_methods_test.dart
+++ fixtures/trait-methods/test/trait_methods_test.dart
@@ -7,58 +7,38 @@ void main() {
       // This test will fail until trait method support is implemented
       // Expected: TraitMethods toString() should return "TraitMethods(yo)"
 
-      // final m = TraitMethods("yo");
-      // expect(m.toString(), equals("TraitMethods(yo)"));
-
-      // Skip test until trait method support is implemented
-      skip(
-        'Blocked by trait method support: [Traits=(Display, Debug, Eq, Hash)]',
-      );
+      final m = TraitMethods("yo");
+      expect(m.toString(), equals("TraitMethods(yo)"));
     });
 
     test('debug trait (representation)', () {
       // This test will fail until trait method support is implemented
       // Expected: TraitMethods debug representation should return 'TraitMethods { val: "yo" }'
 
-      // final m = TraitMethods("yo");
-      // expect(m.debugString(), equals('TraitMethods { val: "yo" }'));
-
-      // Skip test until trait method support is implemented
-      skip(
-        'Blocked by trait method support: [Traits=(Display, Debug, Eq, Hash)]',
-      );
+      final m = TraitMethods("yo");
+      expect(m.debugString(), equals('TraitMethods { val: "yo" }'));
     });
 
     test('eq trait (equality)', () {
       // This test will fail until trait method support is implemented
       // Expected: TraitMethods objects should be equal if they have the same value
 
-      // final m1 = TraitMethods("yo");
-      // final m2 = TraitMethods("yo");
-      // final m3 = TraitMethods("yoyo");
-      //
-      // expect(m1, equals(m2));
-      // expect(m1, isNot(equals(m3)));
+      final m1 = TraitMethods("yo");
+      final m2 = TraitMethods("yo");
+      final m3 = TraitMethods("yoyo");
 
-      // Skip test until trait method support is implemented
-      skip(
-        'Blocked by trait method support: [Traits=(Display, Debug, Eq, Hash)]',
-      );
+      expect(m1, equals(m2));
+      expect(m1, isNot(equals(m3)));
     });
 
     test('hash trait (hashability)', () {
       // This test will fail until trait method support is implemented
       // Expected: TraitMethods objects should be hashable and usable as map keys
 
-      // final map = <TraitMethods, String>{};
-      // final m = TraitMethods("m");
-      // map[m] = "m";
-      // expect(map.containsKey(m), isTrue);
-
-      // Skip test until trait method support is implemented
-      skip(
-        'Blocked by trait method support: [Traits=(Display, Debug, Eq, Hash)]',
-      );
+      final map = <TraitMethods, String>{};
+      final m = TraitMethods("m");
+      map[m] = "m";
+      expect(map.containsKey(m), isTrue);
     });
   });
 
@@ -67,14 +47,9 @@ void main() {
       // This test will fail until proc-macro support is implemented
       // Expected: ProcTraitMethods should behave like TraitMethods but using proc-macros
 
-      // final m = ProcTraitMethods("yo");
-      // expect(m.toString(), equals("ProcTraitMethods(yo)"));
-      // expect(m.debugString(), equals('ProcTraitMethods { val: "yo" }'));
-
-      // Skip test until proc-macro support is implemented
-      skip(
-        'Blocked by proc-macro support: #[uniffi::export(Debug, Display, Eq, Hash)]',
-      );
+      final m = ProcTraitMethods("yo");
+      expect(m.toString(), equals("ProcTraitMethods(yo)"));
+      expect(m.debugString(), equals('ProcTraitMethods { val: "yo" }'));
     });
   });
 }
