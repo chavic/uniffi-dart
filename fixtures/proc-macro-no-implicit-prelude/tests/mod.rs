061d1789848df6f50eec2e9de6b31e02b27fb0cf
chavic <dev.chavic@gmail.com>
2025-07-21 19:54:03 +0200

    initial proc marco test with no prelude

---
+++ fixtures/proc-macro-no-implicit-prelude/tests/mod.rs
@@ -0,0 +1,7 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_proc_macro_no_implicit_prelude() {
+        uniffi_dart::testing::run_test("proc_macro_no_implicit_prelude", None).unwrap();
+    }
+} 
