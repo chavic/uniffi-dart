9880b08045de9b0a9fe021ba5f8dbdb864ec61a9
chavic <dev.chavic@gmail.com>
2025-07-28 15:41:03 +0200

    using workspace dependancy

--- fixtures/docstring/tests/mod.rs
+++ fixtures/docstring/tests/mod.rs
@@ -2,6 +2,6 @@
 mod tests {
     #[test]
     fn test_docstring() {
-        uniffi_dart::testing::run_test("docstring", None).unwrap();
+        uniffi_dart::testing::run_test("docstring", "src/api.udl", None).unwrap();
     }
 }
